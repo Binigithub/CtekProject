761b9cf8e2a8e77a501554dd21955efc4ea2a83d
Binigithub <42274807+Binigithub@users.noreply.github.com>
2019-01-08 20:42:13 -0600

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,46 +1,131 @@
 exports.config = {
 
-    //address of our selenium server    
-    
-    //seleniumAddress: 'http://localhost:4444/wd/hub',
-    directConnect : true,
-    
-    //which browser would you want to use for your tests?
-    
-    capabilities: {
+	//specs: ['../Tests/demo.spec.js'],
+
+
+
+	suites:{
+
+
+
+		smoke:['../Tests/demo.spec.js'],
+
+		regression:['../Tests/*.spec.js']
+
+	},
+
+ 
+
+	sauceUser: process.env.SAUCE_USERNAME,
+
+	sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+
+
+	// restartBrowserBetweenTests: true,
+
+
+
+    multiCapabilities: [
+
+		{
+
+        browserName: 'firefox',
+
+        version: '61.0',
+
+        platform: 'macOS 10.14',
+
+        name: "firefox-tests",
+
+        shardTestFiles: true,
+
+        maxInstances: 25
+
+	}, 
+
+	{
+
         browserName: 'chrome',
-    chromeOptions:{
-        args:[
-            '--start-fullscreen'
-        ]
-    },
-    sharedTestFiles:true,
-    maxInstances:2
-},
-    
-    // multiCapabilities:[
-    //     {'browserName':'chrome'},
-    //     {'browserName':'firefox'}
 
-    // ],
-    //the name of your test scripts file
-    
-    specs: ['spec.js'],
-    
-    //which BDD framework we're going to use
-    framework: 'jasmine2',
-    
-    jasmineNodeOpts: {
-    
-        showColors: true,
-    
-        defaultTimeoutInterval: 30000
-        
-    
-    }
-    
-    };
+        version: '70.0',
+
+        platform: 'Windows 10',
+
+        name: "chrome-tests",
+
+        shardTestFiles: true,
+
+        maxInstances: 25
+
+    }],
 
 
 
+	onPrepare: function () {
 
+		// browser.ignoreSynchronization = true;	// if not an Angular app
+
+
+
+		jasmine.getEnv().addReporter({
+
+			specStarted: function(result) {
+
+				browser.getCapabilities().then(function (capabilities) {
+
+					var browserName = capabilities.get("browserName");
+
+					browser.executeScript("sauce:job-name=" + browserName + ":" + result.fullName);
+
+				});
+
+			}
+
+		});
+
+
+
+		var AllureReporter = require('jasmine-allure-reporter');
+
+		jasmine.getEnv().addReporter(new AllureReporter({
+
+		  resultsDir: 'allure-results'
+
+		}));
+
+
+
+	},
+
+
+
+	onComplete: function () {
+
+		var printSessionId = function (jobName) {
+
+			browser.getSession().then(function (session) {
+
+				console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+
+			});
+
+		}
+
+		
+
+		printSessionId("Insert Job Name Here");
+
+	},
+
+
+
+	
+
+
+
+
+
+
+
+};
